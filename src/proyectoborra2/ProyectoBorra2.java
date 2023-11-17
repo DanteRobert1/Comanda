ff59e40c1de981509e1e1b439a13aab8a7398566
DanteRobert1 <al261813@edu.uaa.mx>
2023-11-17 10:05:25 -0600

    commit de inicialización del repositorio

---
+++ src/proyectoborra2/ProyectoBorra2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectoborra2;
+
+/**
+ *
+ * @author Daniel
+ */
+public class ProyectoBorra2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         new Ventana().setVisible(true);
+         
+    }
+}
